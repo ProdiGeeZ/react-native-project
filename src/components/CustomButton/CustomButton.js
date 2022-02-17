d79a146305f088e381d404891cfeb83a294eda14
Saabir <saabirahmed@live.co.uk>
2022-02-17 22:56:38 +0000

    feat(login): add register button

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Text, View, StyleSheet, Pressable } from 'react-native';
 
-const CustomButton = ({ onPress, text}) => (
-    <Pressable onPress={onPress} style={styles.container}>
+const CustomButton = ({ onPress, text, type}) => (
+    <Pressable onPress={onPress} style={[styles.container, styles[`container_${type}`]]}>
         <Text style={styles.text}>{text}</Text>
     </Pressable>
 );
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'orange',
         width: '50%',
         padding: 15,
-        marginVertical: 15,
+        marginVertical: 10,
         alignItems: 'center',
         borderRadius: 20
+    },
+    container_PRIMARY:{
+        backgroundColor: 'orange',
+    },
+    container_SECONDARY:{
+        backgroundColor: 'grey'
     },
     text:{
         fontWeight: 'bold',
--- src/screens/Login/LoginScreen.jsx
+++ src/screens/Login/LoginScreen.jsx
@@ -1,51 +1,61 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { Text, View, Image, StyleSheet, useWindowDimensions } from 'react-native';
-import {API_URL} from '../../../consts.json'
+import { Text, View, Image, StyleSheet, useWindowDimensions, ScrollView } from 'react-native';
+import { API_URL } from '../../../consts.json'
 import Logo from '../../../assets/images/rocket.png'
 import CustomButton from '../../components/CustomButton/CustomButton';
 import CustomInput from '../../components/CustomInput/CustomInput';
 
-const LoginScreen = ({setToken, setId}) => {
+const LoginScreen = ({ setToken, setId }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const { height } = useWindowDimensions();
 
     const onLoginPressed = () => {
-       axios.post(`${API_URL}/login`, {email,password})
-       .then(({data}) => {
-            setToken(data.token);
-            setId(data.id)
-       }).catch((err) => {
-           console.warn(err);
-       })
+        axios.post(`${API_URL}/login`, { email, password })
+            .then(({ data }) => {
+                setToken(data.token);
+                setId(data.id);
+                // TODO: Navigate to next screen
+            }).catch((err) => {
+                //TODO: Validation
+                console.warn(err);
+            })
     }
-    return (
-        <View style={styles.root}>
-            <Image
-                source={Logo}
-                style={[styles.logo, { height: height * 0.2 }]}
-                resizeMode="contain" />
-            <Text style={styles.text}>
-                Spacebook
-            </Text>
-            <CustomInput
-                placeholder='Email'
-                value={email}
-                setValue={setEmail}
-            />
-            <CustomInput
-                placeholder='Password'
-                value={password}
-                setValue={setPassword}
-                secure 
-            />
-           <CustomButton
-            text='Login'
-            onPress={onLoginPressed}/>
-        </View>
 
+
+    return (
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.root}>
+                <Image
+                    source={Logo}
+                    style={[styles.logo, { height: height * 0.2 }]}
+                    resizeMode="contain" />
+                <Text style={styles.text}>
+                    Spacebook
+                </Text>
+                <CustomInput
+                    placeholder='Email'
+                    value={email}
+                    setValue={setEmail}
+                />
+                <CustomInput
+                    placeholder='Password'
+                    value={password}
+                    setValue={setPassword}
+                    secure
+                />
+                <CustomButton
+                    text='Login'
+                    onPress={onLoginPressed}
+                    type='PRIMARY' />
+
+                <CustomButton
+                    text="Don't have an account? Register"
+                    type='SECONDARY' />
+            </View>
+        </ScrollView>
     )
 };
 
