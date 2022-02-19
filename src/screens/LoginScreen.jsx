cd080af714e7913382cf3d1876d2976f0cd11ae5
Saabir <saabirahmed@live.co.uk>
2022-02-19 00:38:49 +0000

    feat(login): updated styling and WIP validation

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,17 +1,16 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { Text, View, Image, StyleSheet, useWindowDimensions, ScrollView } from 'react-native';
-import { API_URL } from '../../consts.json'
+import { Text, View, StyleSheet, ScrollView, ImageBackground, Dimensions } from 'react-native';
+import { Icon, Input, FormControl, WarningOutlineIcon, Button } from 'native-base';
+import { MaterialIcons, AntDesign } from "@expo/vector-icons";
+import { API_URL } from '../../consts.json';
 import Logo from '../../assets/images/rocket.png'
-import CustomButton from '../components/CustomButton/CustomButton';
-import CustomInput from '../components/CustomInput/CustomInput';
 
 const LoginScreen = ({ screenProps, navigation }) => {
 	const { setId, setToken, token } = screenProps;
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
-
-	const { height } = useWindowDimensions();
+	const [error, setError] = useState(false);
 
 	const onLoginPressed = () => {
 		axios.post(`${API_URL}/login`, { email, password })
@@ -21,7 +20,7 @@ const LoginScreen = ({ screenProps, navigation }) => {
 				console.log(`done - heres the token ${token}`);
 				// TODO: Navigate to next screen
 			}).catch((err) => {
-				//TODO: Validation
+				setError(!error);
 				console.warn(err);
 			})
 	}
@@ -31,54 +30,108 @@ const LoginScreen = ({ screenProps, navigation }) => {
 	}
 
 	return (
-		<ScrollView showsVerticalScrollIndicator={false}>
-			<View style={styles.root}>
-				<Image
-					source={Logo}
-					style={[styles.logo, { height: height * 0.2 }]}
-					resizeMode="contain" />
-				<Text style={styles.text}>
-					Spacebook
-				</Text>
-				<CustomInput
-					placeholder='Email'
-					value={email}
-					setValue={setEmail}
-				/>
-				<CustomInput
-					placeholder='Password'
-					value={password}
-					setValue={setPassword}
-					secure
-				/>
-				<CustomButton
-					text='Login'
-					onPress={onLoginPressed}
-					type='PRIMARY' />
-				<Text style={{ width: 50, textAlign: 'center' }}>OR</Text>
-				<CustomButton
-					text="Don't have an account? Register"
-					onPress={redirectToSignUp}
-					type='SECONDARY' />
+		<ScrollView style={styles.container} showsVerticalScrollIndicator={true}>
+			<ImageBackground
+				source={Logo}
+				style={styles.logo}
+			/>
+			<View>
+				<Text style={styles.brandViewText}>Spacebook</Text>
 			</View>
-		</ScrollView>
+
+			<View style={styles.bottomView}>
+				<View style={{ padding: 30 }}>
+					<Text style={styles.heading}>Welcome</Text>
+					<Text>Don't have an account?
+						<Text style={styles.registerTxt}>{' '} Register now</Text>
+					</Text>
+
+					{/* Form inputs */}
+					<View>
+						<FormControl isInvalid={error} style={styles.formControl}>
+							<Input
+								variant='underlined'
+								keyboardType='email-address'
+								InputLeftElement={<Icon as={<MaterialIcons name="person" />} size={5} ml="2" color="muted.400" />}
+								placeholder="Enter Email"
+								onChangeText={text => setEmail(text)}
+							/>
+							<FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+								Email/Password is incorrect
+							</FormControl.ErrorMessage>
+						</FormControl>
+						<FormControl isInvalid={error} style={styles.formControl}>
+							<Input
+								variant='underlined'
+								InputLeftElement={<Icon as={<AntDesign name="lock" />} size={5} ml="2" color="muted.400" />}
+								placeholder="Enter password"
+								onChangeText={text => setPassword(text)}
+							/>
+							<FormControl.HelperText>
+								Must be atleast 6 characters.
+							</FormControl.HelperText>
+							<FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+								Email/Password is incorrect
+							</FormControl.ErrorMessage>
+						</FormControl>
+
+						<View style={styles.button}>
+							<Button rounded style={styles.login}>
+								<Text> Login </Text>
+							</Button>
+						</View>
+					</View>
+				</View>
+			</View>
+		</ScrollView >
 	)
 };
 
 
 const styles = StyleSheet.create({
-	root: {
-		alignItems: 'center',
-		paddingTop: 50,
+	container: {
+		flexDirection: 'column',
+		backgroundColor: '#ff8f73',
+		paddingTop: 25,
+	},
+	brandViewText: {
+		textAlign: 'center',
+		color: 'white',
+		fontSize: 40,
+		fontWeight: 'bold',
+		textTransform: 'uppercase'
+	},
+	button: {
+		height: 100,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	bottomView: {
+		flex: 3,
+		backgroundColor: 'white',
+		borderTopStartRadius: 60,
+		borderTopEndRadius: 60,
+		height: '100%'
+	},
+	formControl: {
+		padding: 10,
+	},
+	heading: {
+		color: '#f27d0c',
+		fontSize: 34,
 	},
 	logo: {
-		width: '70%',
-		maxWidth: 200,
-		maxHeight: 200,
+		height: Dimensions.get('window').height / 2,
 	},
-	text: {
-		paddingBottom: 50,
-		fontSize: 28,
+	login: {
+		alignSelf: 'center',
+		backgroundColor: '#ff8f73',
+		width: Dimensions.get('window').width / 2,
+		justifyContent: 'center'
+	},
+	registerTxt: {
+		color: '#ff8f73',
+		fontStyle: 'italic'
 	}
 });
 
